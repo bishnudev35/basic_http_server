7a59d99273ceab146b8d100ed088f4be905f97b8
bishnudev35 <bishnudevsardar35@gmail.com>
2024-12-10 23:38:53 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,87 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+ 
+const userSchema= new Schema(
+    {
+    username:{
+        type:String,
+        require:true,
+        uniqued:true,
+        lowercase:true,
+        trim:true,
+        index:true,
+    },
+   email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+       
+    },
+  fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true,
+    },
+    avatar:{
+        type:String,
+        required:true,
+    },
+  coverImage:{
+        type:String,
+       
+    },
+   watchHistory:[{
+    type:Schema.Types.ObjectId,
+    ref:"video"
+   }],
+   password:{
+    type:String,
+    required:[true,'Password is required'],
+   },
+   refreshToken:{
+    type:String
+   }
+},{
+    timestamps:true
+})
+
+userSchema.pre("save",async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+userSchema.methods.isPasswordCorrect=async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.generateAcessToken=function(){
+   return jwt.sign({
+        _id: this.id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+    },
+    process.env.ACESS_TOKEN_SECRER,
+    {
+        expiresIn: process.env.ACESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id: this.id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
+export const User= mongoose.model("User",userSchema)
